7dabd2e3d9842740c92d00e4562c5cbfdf8e9460
nem4tgf <143236099+nem4tgf@users.noreply.github.com>
2025-01-08 12:32:52 +0700

    up admin

---
+++ admin/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+
+const routes: Routes = [{path : '', component : HomeComponent},
+  {path : 'login', component : LoginComponent},
+  {path : 'recipes', component : RecipesComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ admin/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { UsersComponent } from './components/users/users.component';
+import { OrderComponent } from './components/order/order.component';
+import { FeedbacksComponent } from './components/feedbacks/feedbacks.component';
+import { NewRecipeComponent } from './components/new-recipe/new-recipe.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RecipesComponent,
+    UsersComponent,
+    OrderComponent,
+    FeedbacksComponent,
+    NewRecipeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
