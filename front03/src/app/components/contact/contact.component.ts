a6e23f2f5c1fb1cc153bfa8e39ac5c819949baf8
Hồng Quân <117747610+dnghngqun@users.noreply.github.com>
2025-01-11 10:49:06 +0700

    update feedback again

--- front03/src/app/components/contact/contact.component.ts
+++ front03/src/app/components/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {NavigationEnd, Router} from "@angular/router";
 import {FeedbackService} from "../../services/feedback.service";
+import Swal from "sweetalert2";
+import {LoadingService} from "../../services/loading.service";
 
 @Component({
   selector: 'app-contact',
@@ -8,13 +10,15 @@ import {FeedbackService} from "../../services/feedback.service";
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  constructor(private router: Router, private feedbackService: FeedbackService) {
+  constructor(private router: Router,
+              private feedbackService: FeedbackService,
+              private loadingService:LoadingService) {
   }
 
-  email!: string;
-  title!: string;
-  content!: string;
-  fullname!: string;
+  email: string = '';
+  title: string = '';
+  content: string = '';
+  fullname: string = '';
   ngOnInit() {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
@@ -25,9 +29,30 @@ export class ContactComponent implements OnInit {
   textMessageErr: string = '';
 
   handleSendFeedback() {
-    if(!this.email || !this.title || !this.content || !this.fullname) {
+    console.log(this.email, this.title, this.content, this.fullname);
+    if(this.email != '' && this.title!= '' && this.content!= '' && this.fullname!= '') {
       // send feedback
-      this.feedbackService.sendFeedback(this.fullname, this.email, this.title, this.content);
+      this.loadingService.setGlobalLoading(true);
+      this.feedbackService.sendFeedback(this.fullname, this.email, this.title, this.content).subscribe(
+        (res) => {
+          this.loadingService.setGlobalLoading(false);
+          Swal.fire({
+            title: "Thank you!",
+            text: "Thanks your feedback!",
+            icon: "success"
+          });
+        },
+        (err) => {
+          this.loadingService.setGlobalLoading(false);
+          console.log(err);
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong! Please try again later.",
+          });
+        }
+      );
+      return;
     }
     this.textMessageErr = 'Please fill in all fields';
     console.log('Please fill in all fields');
--- front03/src/app/services/feedback.service.ts
+++ front03/src/app/services/feedback.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {API} from "./API";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class FeedbackService {
 
   constructor(private http:HttpClient) { }
 
-  sendFeedback(fullname:string, email:string, title:string, content:string) {
-    this.http.post(`http://localhost:5211/${API.SEND_FEEDBACK}`, {fullname, email, title, content});
+  sendFeedback(fullname:string, email:string, title:string, content:string): Observable<any>{
+   return this.http.post(`http://localhost:5211/${API.SEND_FEEDBACK}`, {fullname, email, title, content});
   }
 }
